92e031d6abe07fa7fce1e4a4618a8870d9f1effb
ngay <nathanaelwgay@netscape.net>
2016-11-21 15:07:53 -0500

    Created package with 2 data sets

---
+++ R/BondFunds.R
@@ -0,0 +1,19 @@
+#' Bond Funds
+#'
+#' Select qualities of 180 bond funds.
+#' \itemize{
+#' \item FundNumber
+#' \item Type
+#' \item Assets
+#' \item Fees
+#' \item ExpenseRatio
+#' \item Return2008
+#' \item 3YearReturn
+#' \item 5YearReturn
+#' \item Risk
+#' }
+#'
+#' @format A data frame with 9 columns and 180 entries.
+#' @source Levine, David, David Stephan, and Kathryn Szabat.  Statistics for Managers Using Microsoft EXCEL, Seventh Edition.  New York: Pearson, 2014.
+#' @name BondFunds
+NULL
